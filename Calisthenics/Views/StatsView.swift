059dd11de5f69cc81a3f24e28922ae7bb8eeafe4
Jared Jones <84288718+JaredDanielJones@users.noreply.github.com>
2023-03-28 15:53:48 -0500

    group by date

--- Calisthenics/Views/StatsView.swift
+++ Calisthenics/Views/StatsView.swift
@@ -11,51 +11,86 @@ struct StatsView: View {
     let moc = PersistenceController.shared.container.viewContext
     @FetchRequest(sortDescriptors: [NSSortDescriptor(key: "timestamp", ascending: false)]) var logs: FetchedResults<Log>
     @Environment(\.editMode) private var editMode
-
     
-    var todayMinutes: String {
+    @State private var expandedSections: [Date] = []
+    
+    var today: Date {
+        Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: Date())!
+    }
 
-        var todaySeconds = 0
-        for log in logs {
-            if Calendar.current.isDateInToday(log.timestamp!) {
-                todaySeconds += Int(log.duration)
-            }
-        }
-        let minutes = todaySeconds / 60
-            let seconds = todaySeconds % 60
-            let formatedMinutes = String(format: "%01d:%02d", minutes, seconds)
-        
-        return formatedMinutes
+    var groupedLogs: [Date: [Log]] {
+        Dictionary(grouping: logs) { dateWithoutTime(from: $0.timestamp!) }
     }
     
+    var sortedDates: [Date] {
+        groupedLogs.keys.sorted(by: { $0 > $1 })
+    }
+
+    func dateWithoutTime(from date: Date) -> Date {
+        let components = Calendar.current.dateComponents([.year, .month, .day], from: date)
+        return Calendar.current.date(from: components)!
+    }
+
+    func dateString(from date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        return dateFormatter.string(from: date)
+    }
+
     var body: some View {
         VStack {
-            Text(todayMinutes)
-//            ScrollView {
-                List {
-                    ForEach(logs, id:\.id) { log in
-                        HStack {
-                            Text(log.exercise!)
-                            Spacer()
-                            Text("\(log.duration)")
-                                .foregroundColor(.secondary)
+            List {
+                ForEach(sortedDates, id: \.self) { date in
+                    Section(header: HStack {
+                        Text(dateString(from: date))
+                            .font(.headline)
+                        Spacer()
+                        Image(systemName: expandedSections.contains(date) ? "chevron.down" : "chevron.right")
+                            .foregroundColor(.gray)
+                            .onTapGesture {
+                                withAnimation {
+                                    if expandedSections.contains(date) {
+                                        expandedSections.removeAll { $0 == date }
+                                    } else {
+                                        expandedSections.append(date)
+                                    }
+                                }
+                            }
+                    }) {
+                        if expandedSections.contains(date) {
+                            ForEach(groupedLogs[date]!, id: \.id) { log in
+                                HStack {
+                                    Text(log.exercise!)
+                                    Spacer()
+                                    Text("\(log.duration)")
+                                        .foregroundColor(.secondary)
+                                }
+                            }
+                            .onDelete(perform: { offsets in
+                                deleteTasks(offsets: offsets, date: date)
+                            })
                         }
                     }
-                    .onDelete(perform: deleteTasks)
                 }
-//            }
-            
-        }
-    }
-    
-    private func deleteTasks(offsets: IndexSet) {
-            withAnimation {
-                offsets.map { logs[$0] }.forEach(moc.delete)
-                try? moc.save()
             }
         }
-    
+        .onAppear {
+            if let todayLogs = groupedLogs[today] {
+                expandedSections = [today]
+            }
+        }
+    }
+
+    private func deleteTasks(offsets: IndexSet, date: Date) {
+        withAnimation {
+            let logsToDelete = groupedLogs[date]!
+            offsets.map { logsToDelete[$0] }.forEach(moc.delete)
+            try? moc.save()
+        }
+    }
 }
+
+
 
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
