9252a3e1b4b124ece6eb36fdfd220d45dfc6933d
Jared Jones <84288718+JaredDanielJones@users.noreply.github.com>
2024-03-09 18:15:48 -0600

    working again!

--- Calisthenics/CalisthenicsApp.swift
+++ Calisthenics/CalisthenicsApp.swift
@@ -10,14 +10,25 @@ import SwiftData
 
 @main
 struct CalisthenicsApp: App {
-    
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Log.self,
+            Exercise.self,
+            Muscle.self
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(for: Log.self)
-        .modelContainer(for: Exercise.self)
-        .modelContainer(for: Muscle.self)
+        .modelContainer(sharedModelContainer)
     }
 }
